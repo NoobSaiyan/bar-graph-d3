09358fadf7cf08699b1beb7afbfaade63be27b4f
NoobSaiyan <ms19961998@gmail.com>
2020-12-17 14:05:58 +0530

    basic setup with data and button to change data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
+const datas = [
+  [10, 30, 40, 20],
+  [10, 40, 30, 20, 50, 10],
+  [60, 30, 40, 20, 30],
+]
+let i = 0
 function App() {
+  const [data, setData] = useState<number[]>([])
+
+  useEffect(() => {
+    changeData()
+  }, [])
+
+  const changeData = () => {
+    setData(datas[i++])
+    if (i === datas.length) i = 0
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Graphs with React</h2>
+      <button onClick={changeData}>Change Data</button>
+      <h3>{data}</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
