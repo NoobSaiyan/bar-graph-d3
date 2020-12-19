f4acb4292cd009dd3c0cc74de1324fc7f6faf1ba
NoobSaiyan <ms19961998@gmail.com>
2020-12-19 19:01:17 +0530

    graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
+import BarChart from './charts/BarChart'
 
 const datas = [
-  [10, 30, 40, 20],
-  [10, 40, 30, 20, 50, 10],
-  [60, 30, 40, 20, 30],
+  [10, 30, 40, 20, 98, 43, 24, 54, 65, 67, 57, 6],
+  [10, 40, 30, 20, 50, 10, 34, 54, 65],
+  [60, 30, 40, 20, 30, 43, 45, 56, 7, 87, 8],
 ]
 let i = 0
+
 function App() {
   const [data, setData] = useState<number[]>([])
 
@@ -22,7 +24,7 @@ function App() {
     <div className='App'>
       <h2>Graphs with React</h2>
       <button onClick={changeData}>Change Data</button>
-      <h3>{data}</h3>
+      <BarChart width={600} height={400} data={data} />
     </div>
   )
 }
---
+++ src/charts/BarChart.tsx
@@ -0,0 +1,66 @@
+import * as d3 from 'd3'
+import React, { useRef, useEffect } from 'react'
+
+interface props {
+  width: number
+  height: number
+  data: number[]
+}
+
+const BarChart: React.FC<props> = ({ width, height, data }) => {
+  const ref = useRef<SVGSVGElement>(null)
+
+  useEffect(() => {
+    d3.select(ref.current)
+      .attr('width', width)
+      .attr('height', height)
+      .style('border', '1px solid black')
+      .style('margin-top')
+  })
+
+  useEffect(() => {
+    const draw = () => {
+      const svg = d3.select(ref.current)
+      let selection = svg.selectAll('rect').data(data)
+      let yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data) || 0])
+        .range([0, height - 100])
+
+      selection
+        .transition()
+        .duration(300)
+        .attr('height', (d) => yScale(d))
+        .attr('y', (d) => height - yScale(d))
+
+      selection
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => i * 45)
+        .attr('y', (d) => height)
+        .attr('width', 40)
+        .attr('height', 0)
+        .attr('fill', 'orange')
+        .transition()
+        .duration(300)
+        .attr('height', (d) => yScale(d))
+        .attr('y', (d) => height - yScale(d))
+
+      selection
+        .exit()
+        .transition()
+        .duration(300)
+        .attr('y', (d) => height)
+        .attr('height', 0)
+        .remove()
+    }
+    draw()
+  }, [data, height])
+
+  return (
+    <div className='chart'>
+      <svg ref={ref} />
+    </div>
+  )
+}
+export default BarChart
